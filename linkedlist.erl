463ba153ffd4b06f3c7f97b9f1c792323fbfb93a
louis <l.colin@student.uclouvain.be>
2020-11-21 16:05:35 +0100

    Debug sender and receiver

--- linkedlist.erl
+++ linkedlist.erl
@@ -1,36 +1,40 @@
 -module(linkedlist).
--export([add/2,getNeighbors/2, receiver/2, sender/1, node/2, node_create/1]).
+-export([add/2,getNeighbors/2, receiver/2, sender/1, node/2, node_create/1, create_list_node/1, node_initialisation/1]).
 
 
 
 
-add(ID,[])->[{ID,[]}];
-add(ID,[{IDprev,List_neigh_prev}|T])->[{ID,[IDprev]},{IDprev,lists:append([ID],List_neigh_prev)}|T].
+add(ID,[])->[#{id => ID, list_neighbors => []}];
+add(ID,[ #{id := IDprev, list_neighbors := List_neigh_prev} |T ])->
+  [#{id =>ID, list_neighbors => [#{id_neighbors=>IDprev, age_neighbors=>0}]}, #{id => IDprev, list_neighbors => lists:append([#{id_neighbors=>ID,age_neighbors=>0}],List_neigh_prev)  } |T].
 
 
 getNeighbors(IDNode,[])-> "error, IDNode is not in the given list";
-getNeighbors(IDNode, [{IDNode,List_neigh}|T])-> List_neigh;
-getNeighbors(IDNode, [{ID,List_neigh}|T])->getNeighbors(IDNode, T).
+getNeighbors(IDNode, [#{id := IDNode,list_neighbors:= List_neigh}|T])-> List_neigh;
+getNeighbors(IDNode, [H|T])->getNeighbors(IDNode, T).
 
 receiver(View, IDParent)->
   receive
-    {IDsender, List_neigh}->
-      IDParent ! {"view", lists:append(List_neigh,View)},
-      receiver(lists:append(List_neigh,View),IDParent)
+    #{idsender := IDsender, list_neighbors := List_neigh}-> %le receiver recoit une view d'un autre noeud. Pour le moment, il l'append a sa list de view
+      io:format("time receive in the receiver~n", []),
+      IDParent ! #{message =>"view", view => lists:append(List_neigh,View)}, %mise a jour de la view chez le noeud parent
+      receiver(lists:append(List_neigh,View), IDParent)
     end.
 
 sender(IDParent)->
   receive
-    [{ID, Age}|T]->
-       ID ! {self(), T},
-       sender(IDParent)
-     end.
+    [#{id_neighbors := ID, age_neighbors:= Age}|T]-> % le sender va devoir envoyer un message a un autre node. Pour le moment, il l'envoie au premier noeud de la list
+      ID ! #{idsender=> self(), list_neighbors=> T}, %io:format("time receive in the sender~n", [])
+      io:format("time receive in the sender~n", [])
+    end,
+    sender(IDParent).
 
 node(View, IDsender)->
   receive
-    {"view" , New_View}-> node(New_View);
-    {"time"}-> IDsender ! View
-  end.
+    #{message := "view" , view := New_View}-> node(New_View); %message recu de la prt du receiver => mise a jour de la view
+    #{message := "time"}-> IDsender ! View  %message recu du main thread => le sender doit envoyer un message a un noeud voisin
+  end,
+  node(View,IDsender).
 
 
 
@@ -39,3 +43,14 @@ node_create(View)->
   spawn(linkedlist, receiver, [View,self()]),
   IDsender = spawn(linkedlist, sender, [self()]),
   node(View, IDsender).
+
+
+create_list_node(NbrNode)->create_list_node(NbrNode,[]).
+create_list_node(0,Acc)->lists:reverse(Acc);
+create_list_node(NbrNode,List)-> create_list_node(NbrNode-1, add(NbrNode, List)).
+
+
+node_initialisation(A)->node_initialisation(A,[]).
+node_initialisation([],Acc)-> Acc;
+node_initialisation([#{id := ID, list_neighbors := List_neigh} |T], Acc)->
+  node_initialisation(T, lists:append( [spawn(linkedlist, node_create, [List_neigh])] , Acc) ).
--- test.erl
+++ test.erl
@@ -1,6 +1,8 @@
 -module(test).
--export([test_function/1]).
+-export([test_function/0]).
 -import (linkedlist, [add/2,getNeighbors/2, receiver/2, sender/1, node/2, node_create/1]).
 
-test_function(Nbr)->
-  add(Nbr,[]).
+test_function()->
+  List_node = linkedlist:create_list_node(5),
+  [H|T] = linkedlist:node_initialisation(List_node),
+  H ! #{message => "time"}.
